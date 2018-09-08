71174948e87ea8b54be5852484e5ad6b5030f175
Richard Kiss <him@richardkiss.com>
2018-09-08 15:12:01 -0700

    Rewrite compiler.

--- opacity/compile.py
+++ opacity/compile.py
@@ -1,5 +1,4 @@
 # compiler
-# with thanks to http://norvig.com/lispy.html
 
 import binascii
 
@@ -34,54 +33,92 @@ def parse_as_keyword(token):
         return v
 
 
-def parse_as_string(token):
-    if len(token) > 1 and (token[:1], token[-1:]) in (("'", "'"), ('"', '"')):
-        return token[1:-1].encode("utf8")
-
-
 def parse_as_hex(token):
     if token[:2].upper() == "0X":
-        return bytes_as_hex(binascii.unhexlify(token[2:]))
+        try:
+            return bytes_as_hex(binascii.unhexlify(token[2:]))
+        except Exception:
+            raise SyntaxError("invalid hex: %s" % token[2:])
 
 
 def parse_as_var(token):
     if token[:1].upper() == "X":
-        return Var(int(token[1:]))
+        try:
+            return Var(int(token[1:]))
+        except Exception:
+            raise SyntaxError("invalid int: %s" % token[1:])
 
 
-def tokenize(chars: str) -> list:
-    "Convert a string of characters into a list of tokens."
-    chars = chars.replace('(', ' ( ').replace(')', ' ) ')
-    lines = chars.split("\n")
-    uncommented_lines = [line.split(" ;", 1)[0] for line in lines]
-    chars = ''.join(uncommented_lines)
-    return chars.split()
+def consume_whitespace(sexp: str, offset):
+    while offset < len(sexp) and sexp[offset].isspace():
+        offset += 1
+    return offset
 
 
-def read_from_tokens(tokens: list):
-    "Read an expression from a sequence of tokens."
-    if len(tokens) == 0:
-        raise SyntaxError('unexpected EOF')
-    token = tokens.pop(0)
-    if token == '(':
-        L = []
-        while tokens[0] != ')':
-            L.append(read_from_tokens(tokens))
-        tokens.pop(0)  # pop off ')'
-        return L
-    elif token == ')':
-        raise SyntaxError('unexpected )')
-    else:
-        for c in [parse_as_string, parse_as_keyword, parse_as_int, parse_as_var, parse_as_hex]:
-            v = c(token)
-            if v is not None:
-                return v
-    raise ValueError("unparsable token: %s" % token)
+def consume_until_whitespace(sexp: str, offset):
+    start = offset
+    while offset < len(sexp) and not sexp[offset].isspace() and sexp[offset] != ")":
+        offset += 1
+    return sexp[start:offset], offset
+
+
+def compile_str(sexp: str, offset):
+    start = offset
+    initial_c = sexp[start]
+    offset += 1
+    while offset < len(sexp) and sexp[offset] != initial_c:
+        offset += 1
+    if offset < len(sexp):
+        return sexp[start+1:offset].encode("utf8"), offset + 1
+    raise SyntaxError("unterminated string starting at %d: %s" % (start, sexp[start:]))
+
+
+def compile_atom(sexp: str, offset):
+    c = sexp[offset]
+    if c in "\'\"":
+        return compile_str(sexp, offset)
+
+    item, offset = consume_until_whitespace(sexp, offset)
+    for f in [parse_as_int, parse_as_var, parse_as_hex, parse_as_keyword]:
+        v = f(item)
+        if v is not None:
+            return v, offset
+    raise SyntaxError("can't parse %s at %d" % (item, offset))
+
+
+def compile_list(sexp: str, offset):
+    c = sexp[offset]
+    assert c == "("
+    offset += 1
+
+    offset = consume_whitespace(sexp, offset)
+
+    r = []
+    while offset < len(sexp):
+        c = sexp[offset]
+
+        if c == ")":
+            return r, offset + 1
+
+        t, offset = compile_sexp(sexp, offset)
+        r.append(t)
+        offset = consume_whitespace(sexp, offset)
+
+    raise SyntaxError("missing )")
+
+
+def compile_sexp(sexp: str, offset=0):
+    offset = consume_whitespace(sexp, offset)
+
+    if sexp[offset] == "(":
+        return compile_list(sexp, offset)
+
+    return compile_atom(sexp, offset)
 
 
 def compile_text(program: str):
     "Read an expression from a string."
-    return wrap_blobs(read_from_tokens(tokenize(program)))
+    return wrap_blobs(compile_sexp(program)[0])
 
 
 def disassemble_unwrapped(form):
