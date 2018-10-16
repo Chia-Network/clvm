583ea0c79e3e73c2180849becf3ddd941f0b394f
Richard Kiss <him@richardkiss.com>
2018-10-16 11:52:37 -0700

    Remove hash and replace with sha256. Add assert_output.

--- opacity/keywords.py
+++ opacity/keywords.py
@@ -1,3 +1,3 @@
-KEYWORD_FROM_INT = (". choose1 aggsig point_add . pubkey_for_exp hash . equal sha256 reduce +").split()
+KEYWORD_FROM_INT = (". choose1 aggsig point_add assert_output pubkey_for_exp . . equal sha256 reduce +").split()
 
 KEYWORD_TO_INT = {v: k for k, v in enumerate(KEYWORD_FROM_INT)}
--- tests/check_solution_test.py
+++ tests/check_solution_test.py
@@ -6,23 +6,41 @@ from opacity.compile import compile_text
 from opacity.SExp import SExp
 
 
-def do_test(expected_hash, encumber_script, solution_script):
+def do_test(expected_hash, encumber_script, solution, response):
     compiled_script = compile_text(encumber_script)
     script_hash = hashlib.sha256(compiled_script).digest()
     assert binascii.hexlify(script_hash) == expected_hash.encode("utf8")
-    underlying_solution = compile_text(solution_script)
+    underlying_solution = SExp(solution).as_bin()
     solution_blob = SExp([compiled_script, underlying_solution])
     r = check_solution(script_hash, solution_blob)
-    assert r == 1
+    assert r == response
 
 
 def test_check_solution_1():
     encumber_script = "(equal x0 100)"
     expected_hash = '8d0cb0dc670c3b45789afcaf5e2b59915e7b8c236fe52f6200c4ec1e37a7d504'
-    do_test(expected_hash, encumber_script, "(100)")
+    solution = [100]
+    do_test(expected_hash, encumber_script, solution, 1)
 
 
 def test_check_solution_2():
     encumber_script = "((equal x0 (+ x1 x2)) (equal x1 17) (equal x0 30))"
     expected_hash = 'b0d5588a9a246d384e8d748aa25e66a976874f7b943a82958ee8e9c7e8e09ae7'
-    do_test(expected_hash, encumber_script, "(30 17 13)")
+    solution = [30, 17, 13]
+    do_test(expected_hash, encumber_script, solution, 1)
+
+
+def test_assert_output():
+    encumber_script = "(reduce x0 x1)"
+    expected_hash = '989d86ec65cabb0c5223b4872ca53a443785f615744fbf3a2789486d5b5b5fb2'
+    x0 = compile_text("(assert_output 500 600 700)")
+    x1 = SExp([]).as_bin()
+    solution = [x0, x1]
+    expected = [SExp.from_blob(compile_text("(assert_output 500 600 700)"))]
+    do_test(expected_hash, encumber_script, solution, expected)
+
+    x0 = compile_text("((equal 55 (+ x0 9)) (assert_output 500 600 700) (equal 10025 x1 (+ x2 25)))")
+    x1 = SExp([46, 10025, 10000]).as_bin()
+    solution = [x0, x1]
+    expected = [[SExp.from_blob(compile_text("(assert_output 500 600 700)"))]]
+    do_test(expected_hash, encumber_script, solution, expected)
