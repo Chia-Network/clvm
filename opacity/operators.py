32805e342d3dc6dfe124e23d9b1b0978f0465127
Richard Kiss <him@richardkiss.com>
2018-11-01 10:57:14 -0700

    Refactor out operators.

---
+++ opacity/operators.py
@@ -0,0 +1,83 @@
+import hashlib
+
+from .casts import bls12_381_from_bytes, bls12_381_to_bytes
+from .ecdsa.bls12_381 import bls12_381_generator
+
+from .SExp import SExp
+
+
+S_False = SExp(0)
+S_True = SExp(1)
+
+
+def has_unbound_values(items):
+    return any(not _.is_bytes() for _ in items)
+
+
+def op_and(items):
+    return S_False if S_False in items else S_True
+
+
+def op_pubkey_for_exp(items):
+    def blob_for_item(_):
+        try:
+            return bls12_381_to_bytes(bls12_381_generator * _.as_int())
+        except Exception as ex:
+            return b''
+
+    if len(items) < 1:
+        return S_False
+    return SExp(blob_for_item(items[0]))
+
+
+def op_point_add(items):
+    if len(items) < 1:
+        return S_False
+    p = bls12_381_from_bytes(items[0].as_bytes())
+    for _ in items[1:]:
+        p += bls12_381_from_bytes(_.as_bytes())
+    return SExp(bls12_381_to_bytes(p))
+
+
+def op_add(items):
+    return SExp(sum(_.as_int() for _ in items) & ((1 << 128) - 1))
+
+
+def op_sha256(items):
+    h = hashlib.sha256()
+    for _ in items:
+        h.update(_.as_bytes())
+    return SExp(h.digest())
+
+
+def op_equal(items):
+    if len(items) == 0:
+        return S_True
+    return SExp(0 if any(_ != items[0] for _ in items[1:]) else 1)
+
+
+def op_unwrap(items):
+    try:
+        return SExp.from_blob(items[0].as_bytes())
+    except (IndexError, ValueError):
+        return S_False
+
+
+def op_to_reduce(f_op):
+    def do_f_op(form, bindings, reduce_f):
+        items = [reduce_f(_, bindings, reduce_f) for _ in form[1:]]
+        if has_unbound_values(items):
+            return SExp([form[0], *items])
+        return f_op(items)
+    return do_f_op
+
+
+def all_operators(remap, keyword_to_int):
+    g = globals()
+    d = {}
+    for k, i in keyword_to_int.items():
+        f_op = g.get("op_%s" % remap.get(k, k))
+        if f_op:
+            d[i] = op_to_reduce(f_op)
+
+    return d
--- opacity/reduce.py
+++ opacity/reduce.py
@@ -1,8 +1,5 @@
-import hashlib
-
-from .casts import bls12_381_from_bytes, bls12_381_to_bytes
-from .ecdsa.bls12_381 import bls12_381_generator
 from .keywords import KEYWORD_TO_INT
+from .operators import all_operators
 
 from .SExp import SExp
 
@@ -13,48 +10,6 @@ S_True = SExp(1)
 
 def has_unbound_values(items):
     return any(not _.is_bytes() for _ in items)
-
-
-def op_and(items):
-    return S_False if S_False in items else S_True
-
-
-def op_pubkey_for_exp(items):
-    def blob_for_item(_):
-        try:
-            return bls12_381_to_bytes(bls12_381_generator * _.as_int())
-        except Exception as ex:
-            return b''
-
-    if len(items) < 1:
-        return S_False
-    return SExp(blob_for_item(items[0]))
-
-
-def op_point_add(items):
-    if len(items) < 1:
-        return S_False
-    p = bls12_381_from_bytes(items[0].as_bytes())
-    for _ in items[1:]:
-        p += bls12_381_from_bytes(_.as_bytes())
-    return SExp(bls12_381_to_bytes(p))
-
-
-def op_add(items):
-    return SExp(sum(_.as_int() for _ in items) & ((1 << 128) - 1))
-
-
-def op_sha256(items):
-    h = hashlib.sha256()
-    for _ in items:
-        h.update(_.as_bytes())
-    return SExp(h.digest())
-
-
-def op_equal(items):
-    if len(items) == 0:
-        return S_True
-    return SExp(0 if any(_ != items[0] for _ in items[1:]) else 1)
 
 
 def do_choose1(form, bindings, reduce_f):
@@ -103,13 +58,6 @@ def do_wrap(form, bindings, reduce_f):
     return SExp(item.as_bin())
 
 
-def op_unwrap(items):
-    try:
-        return SExp.from_blob(items[0].as_bytes())
-    except (IndexError, ValueError):
-        return S_False
-
-
 def do_reduce(form, bindings, reduce_f):
     if len(form) < 2:
         return S_False
@@ -125,28 +73,13 @@ def do_recursive_reduce(form, bindings, reduce_f):
     return SExp(form.as_list()[:1] + [reduce_f(_, bindings, reduce_f) for _ in form[1:]])
 
 
-def op_to_reduce(f_op):
-    def do_f_op(form, bindings, reduce_f):
-        items = [reduce_f(_, bindings, reduce_f) for _ in form[1:]]
-        if has_unbound_values(items):
-            return SExp([form[0], *items])
-        return f_op(items)
-    return do_f_op
-
-
 def build_reduce_lookup(remap, keyword_to_int):
     g = globals()
-    d = dict()
+    d = all_operators(remap, keyword_to_int)
     for k, i in keyword_to_int.items():
-        f = None
         if k in remap:
             k = remap[k]
-        if f is None:
-            f = g.get("do_%s" % k)
-        if f is None:
-            f_op = g.get("op_%s" % k)
-            if f_op:
-                f = op_to_reduce(f_op)
+        f = g.get("do_%s" % k)
         if f:
             d[i] = f
 
