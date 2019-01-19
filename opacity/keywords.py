00821b965986b51614c034a404b46b8f60d675e8
Richard Kiss <him@richardkiss.com>
2019-01-18 17:27:13 -0800

    Add "case", to make recursion easier.

--- opacity/keywords.py
+++ opacity/keywords.py
@@ -1,6 +1,7 @@
 KEYWORD_FROM_INT = (
     ". choose1 aggsig point_add assert_output pubkey_for_exp and type equal "
     "sha256 reduce + * - / wrap unwrap list quote quasiquote unquote get env "
-    "cons first rest list type is_null var").split()
+    "case "
+    "cons first rest list type is_null var apply").split()
 
 KEYWORD_TO_INT = {v: k for k, v in enumerate(KEYWORD_FROM_INT)}
