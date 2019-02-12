32a095a1a6ee12726cd44db4f9115d24329a4fd5
Richard Kiss <him@richardkiss.com>
2019-02-12 15:52:31 -0800

    Get rid of tests that depend upon implicit and.

--- tests/check_solution_test.py
+++ tests/check_solution_test.py
@@ -24,8 +24,8 @@ def test_check_solution_1():
 
 
 def test_check_solution_2():
-    encumber_script = "((equal x0 (+ x1 x2)) (equal x1 17) (equal x0 30))"
-    expected_hash = 'b0d5588a9a246d384e8d748aa25e66a976874f7b943a82958ee8e9c7e8e09ae7'
+    encumber_script = "(and (equal x0 (+ x1 x2)) (equal x1 17) (equal x0 30))"
+    expected_hash = '9dde76feb02311c1c9d5f07337e8c99a555b2e0e12ab470d00fd691030dc4fd3'
     solution = [30, 17, 13]
     do_test(expected_hash, encumber_script, solution, 1)
 
@@ -39,7 +39,7 @@ def test_assert_output():
     expected = SExp.from_blob(compile_to_blob("(and (assert_output 500 600 700) 1)"))
     do_test(expected_hash, encumber_script, solution, expected)
 
-    x0 = compile_to_blob("((equal 55 (+ x0 9)) (assert_output 500 600 700) (equal 10025 x1 (+ x2 25)))")
+    x0 = compile_to_blob("(and (equal 55 (+ x0 9)) (assert_output 500 600 700) (equal 10025 x1 (+ x2 25)))")
     x1 = SExp([46, 10025, 10000]).as_bin()
     solution = [x0, x1]
     expected = SExp.from_blob(compile_to_blob("(and (and 1 (assert_output 500 600 700) 1) 1)"))
