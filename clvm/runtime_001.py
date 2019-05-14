32af14069f02d971267fa3f45a44fe2539780bad
Richard Kiss <him@richardkiss.com>
2019-05-14 15:10:46 -0700

    Add as_python, __str__, __repr__ and bls12_381 casts to SExp.

--- clvm/runtime_001.py
+++ clvm/runtime_001.py
@@ -2,8 +2,8 @@ from opacity.Var import Var
 
 from . import core_ops, more_ops
 
-from .casts import int_from_bytes, int_to_bytes
-from .make_eval import make_eval_f
+from .casts import int_from_bytes, int_to_bytes, bls12_381_from_bytes, bls12_381_to_bytes, bls12_381_generator
+from .make_eval import make_eval_f, EvalError
 from .op_utils import operators_for_module
 from .subclass_sexp import subclass_sexp
 
@@ -13,10 +13,26 @@ class mixin:
     def to_atom(class_, v):
         if isinstance(v, int):
             v = int_to_bytes(v)
+        if isinstance(v, bls12_381_generator.__class__):
+            v = bls12_381_to_bytes(v)
         return v
 
     def as_int(self):
         return int_from_bytes(self.as_atom())
+
+    def as_bls12_381(self):
+        return bls12_381_from_bytes(self.as_atom())
+
+    def as_python(self):
+        if isinstance(self.v, self.ATOM_TYPES):
+            return self.v
+        return list(_.as_python()for _ in self.as_iter())
+
+    def __str__(self):
+        return str(self.as_python())
+
+    def __repr__(self):
+        return "%s(%s)" % (self.__class__.__name__, str(self))
 
 
 to_sexp_f = subclass_sexp(mixin, (bytes, Var))
