e89dece9b6b50c57ec517581c59794dd0b2463b6
Richard Kiss <him@richardkiss.com>
2019-08-20 15:45:48 -0700

    Delete obsolete files.

--- opacity/Var.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Var:
-    index: int
-
-    def __repr__(self):
-        return "x%d" % self.index
--- opacity/writer.py
+++
@@ -1,7 +0,0 @@
-# writer
-
-
-def write_tokens(sexp):
-    if sexp.listp():
-        return "(%s)" % ' '.join(write_tokens(_) for _ in sexp.as_iter())
-    return sexp.as_atom()
