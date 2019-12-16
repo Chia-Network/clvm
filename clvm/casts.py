6797e84bb4ab7dcf0def9068c619e439d0314dcc
Richard Kiss <him@richardkiss.com>
2019-12-16 15:27:46 -0800

    Okay, this should actually match how blspy streams.

--- clvm/casts.py
+++ clvm/casts.py
@@ -29,6 +29,8 @@ def bls12_381_from_bytes(blob):
     x = int.from_bytes(ba, byteorder="big", signed=False)
     try:
         points = bls12_381_generator.points_for_x(x)
+        # sort by y value
+        points = sorted(points, key=lambda p: p[1])
         return points[0 if hi_bit else 1]
     except ValueError:
         return bls12_381_generator.infinity()
@@ -39,7 +41,7 @@ def bls12_381_to_bytes(point):
     if x is None:
         return b''
     as_bytes = bytearray(x.to_bytes(length=BLS12_381_POINT_BYTE_COUNT, byteorder="big", signed=False))
-    if y & 1 == 0:
+    if y + y > bls12_381_generator.order():
         as_bytes[0] |= 0x80
     return bytes(as_bytes)
 
