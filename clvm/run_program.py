c72f062c00d33e0e5bf99e6c2d7bc04ca20053d4
Richard Kiss <him@richardkiss.com>
2020-02-28 18:49:34 -0800

    First crack at non-recursive implementation.

--- clvm/run_program.py
+++ clvm/run_program.py
@@ -1,4 +1,8 @@
-from .Eval import Eval
+from .EvalError import EvalError
+
+QUOTE_COST = 1
+ARGS_COST = 1
+DEFAULT_APPLY_COST = 1
 
 
 def make_run_program(operator_lookup, quote_kw, args_kw):
@@ -14,28 +18,107 @@ def make_run_program(operator_lookup, quote_kw, args_kw):
         post_eval_f=None,
     ):
 
-        eval_class = Eval
-        if pre_eval_f or post_eval_f:
+        def eval_atom_op(op_stack, value_stack):
+            pair = value_stack.pop()
+            sexp = pair.first()
+            env = pair.rest()
+            node_index = sexp.as_int()
+            cost = 1
+            while node_index > 1:
+                if node_index & 1:
+                    env = env.rest()
+                else:
+                    env = env.first()
+                node_index >>= 1
+                cost += 1
+            value_stack.append(env)
+            return cost
 
-            class WrappedEval(Eval):
-                def eval(self, sexp, env, current_cost, max_cost):
-                    context = None
-                    if pre_eval_f:
-                        context = pre_eval_f(sexp, env, current_cost, max_cost)
-                    try:
-                        r = super().eval(sexp, env, current_cost, max_cost)
-                    except Exception as ex:
-                        r = (0, sexp.to(("FAIL: %s" % str(ex)).encode("utf8")))
-                        raise
-                    finally:
-                        if post_eval_f:
-                            post_eval_f(context, r)
-                    return r
+        def swap_op(op_stack, value_stack):
+            v2 = value_stack.pop()
+            v1 = value_stack.pop()
+            value_stack.append(v2)
+            value_stack.append(v1)
+            return 0
 
-            eval_class = WrappedEval
+        def cons_op(op_stack, value_stack):
+            v1 = value_stack.pop()
+            v2 = value_stack.pop()
+            value_stack.append(v1.cons(v2))
+            return 0
 
-        eval = eval_class(operator_lookup, quote_kw, args_kw)
+        def eval_op(op_stack, value_stack):
+            pair = value_stack.pop()
+            sexp = pair.first()
+            args = pair.rest()
+            # put a bunch of ops on op_stack
 
-        return eval(program, args, max_cost=max_cost)
+            if not sexp.listp():
+                # sexp is an atom
+                op_stack.append(eval_atom_op)
+                value_stack.append(pair)
+                return 1
+
+            operator = sexp.first()
+            if operator.listp():
+                value_stack.append(operator.cons(args))
+                op_stack.append(eval_op)
+                op_stack.append(eval_op)
+                return 1
+
+            op = operator.as_atom()
+            operand_list = sexp.rest()
+            if op == quote_kw:
+                if operand_list.nullp() or not operand_list.rest().nullp():
+                    raise EvalError("quote requires exactly 1 parameter", sexp)
+                value_stack.append(operand_list.first())
+                return QUOTE_COST
+
+            if op == args_kw:
+                if sexp.nullp() or not sexp.rest().nullp():
+                    raise EvalError("env requires no parameters", sexp)
+                value_stack.append(args)
+                return ARGS_COST
+
+            op_stack.append(apply_op)
+            value_stack.append(operator)
+            for _ in operand_list.as_iter():
+                value_stack.append(_.cons(args))
+                op_stack.append(cons_op)
+                op_stack.append(eval_op)
+                op_stack.append(swap_op)
+            value_stack.append(operator.null())
+            return 1
+
+        def apply_op(op_stack, value_stack):
+            operand_list = value_stack.pop()
+            operator = value_stack.pop()
+            if operator.listp():
+                raise EvalError("internal error", operator)
+
+            f = operator_lookup.get(operator.as_atom())
+            if f:
+                if getattr(f, "needs_eval", 0):
+                    r = f(operand_list, run_program)
+                else:
+                    r = f(operand_list)
+                additional_cost = DEFAULT_APPLY_COST
+                if isinstance(r, (tuple,)):
+                    additional_cost, r = r
+                value_stack.append(r)
+                return additional_cost
+
+            raise EvalError("unimplemented operator", operator)
+
+        op_stack = [eval_op]
+        value_stack = [program.cons(args)]
+        cost = 0
+
+        while op_stack:
+            f = op_stack.pop()
+            cost += f(op_stack, value_stack)
+            if max_cost and cost > max_cost:
+                raise EvalError("cost exceeded", value_stack[-1])
+        return cost, value_stack[-1]
 
     return run_program
