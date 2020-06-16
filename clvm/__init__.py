26b9b74b9edf12744fc6cf8e923e0605e666d484
Richard Kiss <him@richardkiss.com>
2020-06-16 15:59:04 -0700

    Factor out `__version__`.

--- clvm/__init__.py
+++ clvm/__init__.py
@@ -1,16 +1,9 @@
-from pkg_resources import get_distribution, DistributionNotFound
-
 from .SExp import SExp
 from .operators import (  # noqa
     KEYWORD_TO_ATOM,
     KEYWORD_FROM_ATOM,
 )
 from .runtime_001 import run_program  # noqa
-
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    # package is not installed
-    __version__ = "unknown"
+from .version import __version__  # noqa
 
 to_sexp_f = SExp.to  # noqa
