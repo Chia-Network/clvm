a0afa2911156819ab25893acdb1fb3cca457ccf1
Richard Kiss <him@richardkiss.com>
2020-11-30 16:14:59 -0800

    Add some comments.

--- clvm/casts.py
+++ clvm/casts.py
@@ -10,7 +10,9 @@ def int_to_bytes(v):
     if v == 0:
         return b""
     r = v.to_bytes(byte_count, "big", signed=True)
-    while len(r) > 1 and r[0] == (0xff if r[1] & 0x80 else 0):
+    # make sure the string returned is minimal
+    # ie. no leading 00 or ff bytes that are unnecessary
+    while len(r) > 1 and r[0] == (0xFF if r[1] & 0x80 else 0):
         r = r[1:]
     return r
 
