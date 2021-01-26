0e0b940ef75ba721195b95138f12542a3a90ce67
Arvid Norberg <arvid@libtorrent.org>
2021-01-26 23:26:00 +0100

    unit test creating bitmask for most significant bit

---
+++ tests/run_program_test.py
@@ -0,0 +1,22 @@
+import unittest
+
+from clvm.run_program import msb_mask
+
+
+class BitTest(unittest.TestCase):
+
+    def test_msb_mask(self):
+        self.assertEqual(msb_mask(0x0), 0x0)
+        self.assertEqual(msb_mask(0x01), 0x01)
+        self.assertEqual(msb_mask(0x02), 0x02)
+        self.assertEqual(msb_mask(0x04), 0x04)
+        self.assertEqual(msb_mask(0x08), 0x08)
+        self.assertEqual(msb_mask(0x10), 0x10)
+        self.assertEqual(msb_mask(0x20), 0x20)
+        self.assertEqual(msb_mask(0x40), 0x40)
+        self.assertEqual(msb_mask(0x80), 0x80)
+
+        self.assertEqual(msb_mask(0x44), 0x40)
+        self.assertEqual(msb_mask(0x2a), 0x20)
+        self.assertEqual(msb_mask(0xff), 0x80)
+        self.assertEqual(msb_mask(0x0f), 0x08)
