9a6050139bb87e322fd39bc3dc8472e51240b51d
b0mTrady <64801585+b0mTrady@users.noreply.github.com>
2021-04-20 05:26:02 -0400

    Update CLVMObject.py
    Added tiny amount of documentation for __slots__ dunder and typing.Optional for context

--- clvm/CLVMObject.py
+++ clvm/CLVMObject.py
@@ -4,6 +4,7 @@ stored in the heap. The methods here are the only ones required for `run_program
 A native implementation of `run_program` should implement this base class.
 """
 
+# typing provides support for type hints -> https://docs.python.org/3/library/typing.html
 import typing
 
 # Set this to "1" to do a run-time type check
@@ -13,10 +14,20 @@ TYPE_CHECK = 0
 
 
 class CLVMObject:
-
+    '''
+    Slots "Restricts the valid set of attribute names on an object to exactly those names listed." 
+    Second, since the attributes are now fixed, it is no longer necessary to store attributes in an instance dictionary, 
+    so the __dict__ attribute is removed. 
+    
+    This improves performance of the __new__ class system. 
+    
+    -> Slots and __new__ according to Guidio:
+    http://python-history.blogspot.com/2010/06/inside-story-on-new-style-classes.html
+    '''
+    # Optional[X] is a shorthand for Union[X, None]
     atom: typing.Optional[bytes]
     pair: typing.Optional[typing.Tuple["CLVMObject", "CLVMObject"]]
-    __slots__ = ["atom", "pair"]
+    __slots__ = ["atom", "pair"] 
 
     def __new__(class_, v: "SExpType"):
         if isinstance(v, CLVMObject):
