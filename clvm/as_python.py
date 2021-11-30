389efa3fbe65c77600da63c78d29c0866d292754
Richard Kiss <him@richardkiss.com>
2021-11-30 14:38:23 -0800

    Fix failing test.

--- clvm/as_python.py
+++ clvm/as_python.py
@@ -10,24 +10,18 @@ def as_python(sexp):
         right = val_stack.pop()
         if right == b"":
             val_stack.append([left])
+        elif isinstance(right, list):
+            v = [left] + right
+            val_stack.append(v)
         else:
             val_stack.append((left, right))
-
-    def _extend_list(op_stack, val_stack):
-        left = [val_stack.pop()]
-        right = val_stack.pop()
-        left.extend(right)
-        val_stack.append(left)
 
     def _as_python(op_stack, val_stack):
         t = val_stack.pop()
         pair = t.as_pair()
         if pair:
             left, right = pair
-            if right.listp():
-                op_stack.append(_extend_list)
-            else:
-                op_stack.append(_make_tuple)
+            op_stack.append(_make_tuple)
             op_stack.append(_as_python)
             op_stack.append(_roll)
             op_stack.append(_as_python)
