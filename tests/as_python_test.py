3fb4ee5c8cde47cad3b6d86175b68ccbbf4519dd
Kyle Altendorf <sda@fstab.net>
2023-12-06 19:48:06 -0500

    Update as_python_test.py

--- tests/as_python_test.py
+++ tests/as_python_test.py
@@ -168,14 +168,14 @@ class AsPythonTest(unittest.TestCase):
 
     def test_invalid_type(self):
         with self.assertRaises(ValueError):
-            s = SExp.to(dummy_class)
+            SExp.to(dummy_class)
 
     def test_invalid_tuple(self):
         with self.assertRaises(ValueError):
-            s = SExp.to((dummy_class, dummy_class))
+            SExp.to((dummy_class, dummy_class))
 
         with self.assertRaises(ValueError):
-            s = SExp.to((dummy_class, dummy_class, dummy_class))
+            SExp.to((dummy_class, dummy_class, dummy_class))
 
     def test_clvm_object_tuple(self):
         o1 = CLVMObject(b"foo")
