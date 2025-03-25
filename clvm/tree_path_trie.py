23d3a9e5b28498a87f2f44939671001602b98b6d
Richard Kiss <him@richardkiss.com>
2025-03-25 13:53:34 -0700

    fix: Correctly insert TreePath into TreePathTrie nodes

--- clvm/tree_path_trie.py
+++ clvm/tree_path_trie.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import List, Optional
 
-from .tree_path import TreePath, relative_pointer, are_paths_in_order
+from .tree_path import TreePath, relative_pointer, are_paths_in_order, common_ancestor
 from .casts import limbs_for_int
 
 
@@ -21,13 +21,19 @@ class TreePathTrie:
 
     def insert(self, path: TreePath) -> None:
         node = self.root
-        for bit in reversed(bin(path)[3:]):  # Iterate through bits of path, skipping "0b1" prefix
+        for bit in reversed(
+            bin(path)[3:]
+        ):  # Iterate through bits of path, skipping "0b1" prefix
             bit_index = int(bit)
             if bit_index == 0:
                 if node.to_left is None:
-                    node.to_left = TreePath(int(bin(path)[2:], 2)) # This might be wrong
+                    node.to_left = TreePath(
+                        int(bin(path)[2:], 2)
+                    )  # This might be wrong
                 node = TrieNode(node.to_left, None, 0)
             else:
                 if node.to_right is None:
-                    node.to_right = TreePath(int(bin(path)[2:], 2)) # This might be wrong
+                    node.to_right = TreePath(
+                        int(bin(path)[2:], 2)
+                    )  # This might be wrong
                 node = TrieNode(None, node.to_right, 0)
