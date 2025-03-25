e96a29e7dbfa446dc96bc4858e0607555c635f7f
Richard Kiss <him@richardkiss.com>
2025-03-25 13:54:25 -0700

    feat: add tests for TreePathTrie insert functionality

--- tests/tree_path_trie_test.py
+++ tests/tree_path_trie_test.py
@@ -5,18 +5,30 @@ from clvm.tree_path_trie import TreePathTrie
 
 
 class TestTreePathTrie(unittest.TestCase):
-    def test_insert(self) -> None:
+    def test_trivial(self) -> None:
         trie = TreePathTrie()
-        path1 = TreePath(5)
-        path2 = TreePath(6)
-        trie.insert(path1)
+        path2 = TreePath(2)
         trie.insert(path2)
+        assert trie.root.to_left == path2
+        assert trie.root.to_right is None
+        path3 = TreePath(3)
+        trie.insert(path3)
+        assert trie.root.to_left == path2
+        assert trie.root.to_right == path3
 
-    def test_find_shortest_path(self) -> None:
+    def test_deeper(self) -> None:
         trie = TreePathTrie()
-        path1 = TreePath(5)
-        path2 = TreePath(6)
-        trie.insert(path1)
-        trie.insert(path2)
-        shortest_path = trie.find_shortest_path(TreePath(5), 10)
-        self.assertEqual(shortest_path, None)
+        path18 = TreePath(0b10010)
+        trie.insert(path18)
+        assert trie.root.to_left == path18
+        assert trie.root.to_right is None
+        path13 = TreePath(0b1101)
+        trie.insert(path13)
+        assert trie.root.to_left == path18
+        assert trie.root.to_right == path13
+        path11 = TreePath(0b1011)
+        breakpoint()
+        trie.insert(path11)
+        assert trie.root.to_left == path18
+        # this should be an intermediate node
+        assert trie.root.to_right == path11
